f969a1e36acfc3af40a12ddf9cca24e7451a039b
Danielcard99 <dc2651065@gmail.com>
2024-06-27 18:20:17 -0300

    Adicionado mecanismo da ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta!");
+        Integer numeroDaConta = scanner.nextInt();
+        System.out.println("Por favor, digite o número da Agência!");
+        String numeroDaAgencia = scanner.next();
+        System.out.println("Por favor, digite o seu nome!");
+        scanner.nextLine();
+        String nomeDoCliente = scanner.nextLine();
+        System.out.println("Por favor, digite o seu saldo!");
+        Double saldoDaConta = scanner.nextDouble();
+
+        System.out.println(
+                "Olá " + nomeDoCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                        + numeroDaAgencia + ", conta " + numeroDaConta + " e seu saldo " + saldoDaConta
+                        + " já está disponível para saque.");
+
+        scanner.close();
+
     }
 }
